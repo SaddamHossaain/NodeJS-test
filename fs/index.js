59060116de3774456b163e38830298af20fd1690
Md Saddam Hossain <77723902+SaddamHossaain@users.noreply.github.com>
2022-01-11 20:54:54 +0600

    Sync FS System for file read
    Sync FS System for file read

--- fs/index.js
+++ fs/index.js
@@ -3,11 +3,18 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
     if(req.url=="/") {
+        //Async fs system
         fs.readFile('home.html', function(error, data) {
             res.writeHead(200, {'Content-Type':'text/html'})
             res.write(data)
             res.end()
         })
+    }else if(req.url=="/as") {
+        //Sync fs system
+        let mydata = fs.readFileSync('home.html')
+        res.writeHead(200, {'Content-Type':'text/html'})
+        res.write(mydata)
+        res.end()
     }
 })
 
